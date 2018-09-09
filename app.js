057fa7d408195702e2273e5e7fd437f2b1575c50
binamkayastha <binamkayastha@gmail.com>
2018-09-09 14:46:31 -0400

    Change chart to be time-based

--- app.js
+++ app.js
@@ -5,16 +5,38 @@ new Vue({
   }
 })
 
-new Chartist.Line('.ct-chart', {
-  labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+var chart = new Chartist.Line('.ct-chart', {
   series: [
-    [12, 9, 7, 8, 5],
-    [2, 1, 3.5, 7, 3],
-    [1, 3, 4, 5, 6]
+    {
+      name: 'series-1',
+      data: [
+        {x: new Date(143134652600), y: 53},
+        {x: new Date(143234652600), y: 40},
+        {x: new Date(143340052600), y: 45},
+        {x: new Date(143366652600), y: 40},
+        {x: new Date(143410652600), y: 20},
+        {x: new Date(143508652600), y: 32},
+        {x: new Date(143569652600), y: 18},
+        {x: new Date(143579652600), y: 11}
+      ]
+    },
+    {
+      name: 'series-2',
+      data: [
+        {x: new Date(143134652600), y: 53},
+        {x: new Date(143234652600), y: 35},
+        {x: new Date(143334652600), y: 30},
+        {x: new Date(143384652600), y: 30},
+        {x: new Date(143568652600), y: 10}
+      ]
+    }
   ]
 }, {
-  fullWidth: true,
-  chartPadding: {
-    right: 40
+  axisX: {
+    type: Chartist.FixedScaleAxis,
+    divisor: 5,
+    labelInterpolationFnc: function(value) {
+      return moment(value).format('MMM D');
+    }
   }
 });
